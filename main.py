ecb27f8f39d5bfb4104e85afacb0df5d90bf1cef
Вера <vera.vinichenki30.04@gmail.com>
2025-10-29 13:37:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import random
 import xml.etree.ElementTree as ET
 
 
+MIN_LEN = 30
+MIN_YEAR = 1991
+MAX_YEAR = 1995
 with open('books-en.csv', encoding='cp1251') as file:
     reader = list(csv.DictReader(file, delimiter=';'))
 
 
 # task1
-count = sum(len(row["Book-Title"]) > 30 for row in reader)
+count = sum(len(row["Book-Title"]) > MIN_LEN for row in reader)
 
 
 #task 2
@@ -17,10 +20,10 @@ filter = []
 for row in reader:
     try:
         year = int(row["Year-Of-Publication"])
-        if author in row["Book-Author"] and 1991 <= year <= 1995:
+        if author in row["Book-Author"] and MIN_LEN <= year <= MAX_YEAR:
             filter.append(row)
-    except:
-        pass
+    except (KeyError, ValueError):
+        print('Error')
 
 
 #task 3
@@ -33,14 +36,14 @@ with open("bibliography.txt", "w", encoding="utf-8") as f:
 #task 4
 tree = ET.parse('currency.xml')
 root = tree.getroot()
-CharCode_list = []
+charcode_list = []
 for val in root.findall('.//Valute'):
         if int(val.find('Nominal').text) in (10, 100):
-            CharCode_list.append(val.find('CharCode').text)
+            charcode_list.append(val.find('CharCode').text)
+
 
 if __name__ == "__main__":
-   print("Ans 1:", count)
+    print("Ans 1:", count)
     print("Ans 2:", len(filter))
-    print("Ans 3: Библиография успешно сохранена")
-    print("Ans 4:", " ".join(CharCode_list))
-
+    print("Ans 3: Библиография сохранена")
+    print("Ans 4:", " ".join(charcode_list))
